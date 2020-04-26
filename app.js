0666e7351a3a4392f61aa59002ef949aa0c82332
Shystriyhlopchik <Shystriyhlopchik@yandex.ru>
2020-04-26 21:36:15 +0300

    Добавлен crash test

--- app.js
+++ app.js
@@ -27,6 +27,12 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 // логгер запросов
 app.use(requestLogger);
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.post('/signin', loginValidator, login);
 app.post('/signup', userValidator, createUser);
 app.use(helmet());
